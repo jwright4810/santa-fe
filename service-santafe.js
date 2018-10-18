9fbf70fc8d342a60cd30e94a3293c0e62434eda2
Jarett Wright <jwtech8313@gmail.comgit confit --list>
2018-10-17 22:05:01 -0500

    minor changes to wording in tabs and add staff page

--- service-santafe.js
+++ service-santafe.js
@@ -18,15 +18,3 @@ $(document).ready(function() {
     })
   })
 
-  //header transition function
-
-  $(document).ready(function(){
-    $(window).scroll(function(){
-      var scrollTop = $(window).scrollTop();
-      if (scrollTop > 49) {
-          $('body').addClass('header-fixed');
-      } else {
-          $('body').removeClass('header-fixed');
-      }
-    });
-  });
